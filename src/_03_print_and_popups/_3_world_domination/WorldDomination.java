6d77adb6b6a1338b8fb290eddfb93e7cbfbadfd4
root <root@e3eb27881861>
2020-11-06 23:24:29 +0000

    world domination continue

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog("Hey! Do you know how to code?");
+                             
 		// 2. If they say "yes", tell them they will rule the world.
+		
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
+		
 	}
 }
 
